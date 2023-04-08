9ffbc0a87f18166e26c500c37e3d00b6fc3e56b6
CElizOwens <owens.christina@gmail.com>
2023-04-08 14:27:54 -0700

    committing newdatabase

---
+++ server/dataModel.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+
+const PG_URI =
+  'postgres://xfhjvaaf:kaHfdj5HE54NhHzx8WIV6fv59Zp8_E8V@raja.db.elephantsql.com/xfhjvaaf';
+
+// create a new pool here using the connection string above
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
