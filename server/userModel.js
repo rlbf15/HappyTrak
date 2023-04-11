1d76ed0ef3d9ecb3ce6c1a6c51de9b04c790a95a
Elena Atencio <elenaatencio10@gmail.com>
2023-04-12 00:16:45 +0200

    added userModel and dataModel as well as connection to cloud db

--- server/userModel.js
+++ server/userModel.js
@@ -1,1 +1,16 @@
 //this is the space for user model
+ const mongoose = require('mongoose');
+// URI
+// mongodb+srv://velocirabbit:velocirabbit@cluster0.ose86oe.mongodb.net/?retryWrites=true&w=majority
+ 
+  const Schema = mongoose.Schema;
+
+  const userSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    type: String
+  })
+
+  const User = mongoose.model('users', Survey);
+
+  module.exports = Survey;
