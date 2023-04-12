045c7f5c0caff1d7f241bb67335d1f2079df8215
Dylan Hawkins <dylan.andrew.hawkins@gmail.com>
2023-04-11 20:16:50 -0400

    Add structure for pulling chart data from db

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart } from 'chart.js/auto'
 
-function LineChart ({chartData}) {
-    return <Line data = {chartData}/>
+const LineChart = () => {
+
+    const [chartData, setChartData] = useState({});
+    
+    useEffect(() => {
+        fetch('', {
+            method: 'GET',
+        })
+        .then(data => data.json())
+        .then(response => {
+            console.log(response)
+            //update state / setChartData here
+        })
+    }, [])
+
+return (
+    <Line data = {chartData}/>
+    )
 }
 
 export default LineChart;
