72c15e17d20868a8be40315f51956699aa0ba728
John Donato <johnlloyddonato@yahoo.com>
2023-04-11 20:18:03 -0500

    implemented bcrypt, createUser, and verifyUser Middleware

--- server/userModel.js
+++ server/userModel.js
@@ -1,8 +1,11 @@
 //this is the space for user model
  const mongoose = require('mongoose');
+ const bcrypt = require('bcrypt');
 // URI
 // mongodb+srv://velocirabbit:velocirabbit@cluster0.ose86oe.mongodb.net/?retryWrites=true&w=majority
  
+const Salt_Work_Factor = 10;
+
   const Schema = mongoose.Schema;
 
   const userSchema = new Schema({
@@ -11,6 +14,25 @@
     type: String
   })
 
+  // hashing a password before saving it to the database
+userSchema.pre('save', function(next) {
+  //generate a salt
+  bcrypt.genSalt(Salt_Work_Factor, (err, salt) => {
+    if (err) {
+      return next(err);
+    } 
+    // hash the password using our new salt
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      // override the original password with the hashed one
+      this.password = hash;
+      next();
+    });
+  });
+});
+
   const User = mongoose.model('users', Survey);
 
-  module.exports = Survey;
+  module.exports = User;
